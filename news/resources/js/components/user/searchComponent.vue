328d7407f56ecb3d8a1d7649b24b42d4684f9ea4
unknown <raymundoar94@gmail.com>
2020-01-10 15:44:06 -0600

    se agregan estilos de documentos

--- news/resources/js/components/user/searchComponent.vue
+++ news/resources/js/components/user/searchComponent.vue
@@ -92,17 +92,74 @@
         <div v-if="all_documents">
             <paginate name="result" :list="resultados" :per="5" >
             <div class="row">
-                <ul>
-                    <div v-for="(object, index) in paginated('result')" :key="index" class="col-12">
-                        <a :href="'/documentViewpdf/' + object.id" style="color: black !important;">
-                            <li :href="'/documentViewpdf/' + object.id" style="margin-top:2%">
-                                <img :src="object.imgdesmostrativa" :href="'/documentViewpdf/' + object.id" />
-                                <h3>{{ object.nombre }}</h3>
-                                <p>Ver detalle</p>
-                            </li>
-                        </a>            
-                    </div>
-                </ul>
+                <div class="col-md-12 col-lg-8">
+                    <ul>
+                        <div v-for="(object, index) in paginated('result')" :key="index">
+                            <a :href="'/documentViewpdf/' + object.id" style="color: black !important;">
+                                <li :href="'/documentViewpdf/' + object.id" style="margin-top:2%">
+                                    <img :src="object.imgdesmostrativa" :href="'/documentViewpdf/' + object.id" />
+                                    <h3>{{ object.nombre }}</h3>
+                                    <p>Ver detalle</p>
+                                </li>
+                            </a>            
+                        </div>
+                    </ul>
+                </div>
+
+               <div class="col-md-6 col-lg-4">
+					<div class="pl-20 pl-md-0">
+						
+
+						<div class="mtb-50">
+							<h4 class="p-title"><b>Publicaciones destacadas</b></h4>
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-1-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+
+									<h5 class="color-black "><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Danile Palmer,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a><!-- oflow-hidden -->
+							
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-2-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Danile Palmer,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a><!-- oflow-hidden -->
+							
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-3-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Danile Palmer,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a><!-- oflow-hidden -->
+							
+							<a class="oflow-hidden pos-relative mb-20 dplay-block" href="#">
+								<div class="wh-100x abs-tlr"><img src="images/polular-4-100x100.jpg" alt=""></div>
+								<div class="ml-120 min-h-100x">
+									<h5><b>Bitcoin Billionares Hidding in Plain Sight</b></h5>
+									<h6 class="color-lite-black pt-10">by <span class="color-black"><b>Danile Palmer,</b></span> Jan 25, 2018</h6>
+								</div>
+							</a><!-- oflow-hidden -->
+							
+						</div><!-- mtb-50 -->
+						
+						<div class="mtb-50 mb-md-0" >
+							<h4 class="p-title" style="margin-top: 51px;"><b>NEWSLETTER</b></h4>
+							<p class="mb-20">Subscribe to our newsletter to get notification about new updates,
+								information, discount, etc..</p>
+							<form class="nwsltr-primary-1">
+								<input type="text" placeholder="Your email"/>
+								<button type="submit"><i class="ion-ios-paperplane"></i></button>
+							</form>
+						</div><!-- mtb-50 -->
+						
+					</div><!--  pl-20 -->
+
+                 </div> 
             </div> 
             </paginate> 
 
@@ -470,4 +527,13 @@ li:hover {
   cursor: pointer;
 }
 
+
+.p-title{ position: relative; padding-bottom: 20px; margin-bottom: 4px; }
+
+.p-title:after { content:''; position: absolute; bottom: 0; left: 0; width: 100%; height: 1px; background: #ccc; }
+.p-title:before{ content:''; position: absolute; bottom: 0; left: 0; width: 80px; height: 5px; 
+	background: #F9B500; z-index: 1; }
+
+.color-lite-black{ color: #888; }
+.color-black{ color: #111; }
 </style>
