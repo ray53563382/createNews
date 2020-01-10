8975974cf94bf3129984a3a21375dd31709086a3
unknown <raymundoar94@gmail.com>
2020-01-09 23:14:00 -0600

    se agregan estilos

--- news/resources/js/components/user/searchComponent.vue
+++ news/resources/js/components/user/searchComponent.vue
@@ -26,7 +26,7 @@
         <paginate name="result" :list="resultados" :per="9"  tag="div">
  <div class="row">
     <div v-for="(person, index) in paginated('result')" :key="index" class="col-lg-4 col-md-6 col-sm-12" @click="goToDocumentView(person)" style="margin-bottom: 4%;">
-        <article class="card card--1" v-bind:style="{ 'background-image': 'url(' + person.imgdesmostrativa + ')' ,'border-color': 'gray' }">
+        <article class="card card--1" v-bind:style="{ 'background-image': 'url(' + person.imgdesmostrativa + ')' ,'border-color': 'gray' ,'-webkit-box-shadow': '4px 2px 3px 0px rgba(0,0,0,0.84)'}">
             <div class="card__info-hover">
                 <svg class="card__like"  viewBox="0 0 24 24">
                     <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" />
